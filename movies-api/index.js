c2175af5ff4673861bbee7212b5903de4c379c28
Ruoye Qian <20095880@mail.wit.ie>
2021-11-22 14:34:39 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,7 @@ import moviesRouter from './api/movies';
 import moviesRouter1 from './api/genres';
 import './db';
 import './seedData'
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ app.use(express.json());
 
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', moviesRouter1);
+//Users router
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
